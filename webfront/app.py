2cced4cd2e8770fbf028a32ded4daba16edfa31e
daljeet-hcl <daljeet.singh123@gmail.com>
2020-05-17 03:50:48 +0530

    front proxy working successfully as sidecars

--- webfront/app.py
+++ webfront/app.py
@@ -7,21 +7,24 @@ import os
 import urllib.request
 import urllib.parse
 
-
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/index')
 def main():
-    url = os.getenv('BACKEND_URL')
-    f = urllib.request.urlopen(url)
-    res = f.read().decode('utf-8')
-    return " Welcome, frontend is running on : " + socket.gethostname() + "<p> Backend Response is :<p>" + res + "<p> value of BACKEND_URL env variable is " + url 
- 
+    try:
+        url = "http://webapi/webapi/index" #os.getenv('BACKEND_URL')
+        f = urllib.request.urlopen(url)
+        res = f.read().decode('utf-8')
+        hostname = socket.gethostname()
+        host_ip = socket.gethostbyname(hostname) 
+        return "Frontend hostname : " + hostname +" IP Address : " + host_ip + "<p> Backend Response is : <p>" + res + "<p> value of BACKEND_URL env variable is " + url 
+    except (RuntimeError):
+        return "some exception occured at the server" + RuntimeError 
+
 
 @app.route("/health")
 def health():
     return "OK"    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.1',debug="true")
