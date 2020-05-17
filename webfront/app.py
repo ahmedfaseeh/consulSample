1c37674d5de6ed2be94a78e8d12cac259df7929d
daljeet-hcl <daljeet.singh123@gmail.com>
2020-05-17 16:39:06 +0530

    envoy mesh working completely as sidecar

--- webfront/app.py
+++ webfront/app.py
@@ -12,19 +12,28 @@ app = Flask(__name__)
 @app.route('/index')
 def main():
     try:
-        url = "http://webapi/webapi/index" #os.getenv('BACKEND_URL')
+        url = "http://localhost:9000/webapi/index" #os.getenv('BACKEND_URL')
         f = urllib.request.urlopen(url)
         res = f.read().decode('utf-8')
-        hostname = socket.gethostname()
-        host_ip = socket.gethostbyname(hostname) 
-        return "Frontend hostname : " + hostname +" IP Address : " + host_ip + "<p> Backend Response is : <p>" + res + "<p> value of BACKEND_URL env variable is " + url 
+        return hello() + "<p> Backend Response is : <p>" + res + "<p> value of BACKEND_URL env variable is " + url 
     except (RuntimeError):
         return "some exception occured at the server" + RuntimeError 
+
+
+
+@app.route('/hello')
+def hello():
+    hostname = socket.gethostname()
+    host_ip = socket.gethostbyname(hostname) 
+    return "Hello, Frontend hostname : " + hostname +" IP Address : " + host_ip 
+
 
 
 @app.route("/health")
 def health():
     return "OK"    
+
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',debug="true")
